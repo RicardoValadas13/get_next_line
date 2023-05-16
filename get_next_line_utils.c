9e9630c66274fcd4cffd86eafe7c87580d1ce51e
Ricardo Valadas <ricardovaladas@Air-de-Ricardo.Home>
2023-05-16 20:12:55 +0100

    WhithoutBonus

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -15,6 +15,8 @@ int	ft_strlen(const char *s)
 	int	len;
 
 	len = 0;
+	if (!s)
+		return (0);
 	while (s[len] && s[len] != '\n')
 		len++;
 	if (s[len] == '\n')
@@ -37,13 +39,11 @@ int	check_newline(char *buff)
 			while (j < BUFFER_SIZE)
 			{
 				buff[j - (i + 1)] = buff[j];
-				buff[j] = '\0';
-				j++;
+				buff[j++] = '\0';
 			}
 			return (1);
 		}
-		buff[i] = '\0';
-		i++;
+		buff[i++] = '\0';
 	}
 	return (0);
 }
@@ -53,14 +53,10 @@ char	*thats_line(char *s1, char *s2)
 	char *str3;
 	size_t i;
 	size_t j;
-	size_t len_s1;
 
 	i = 0;
 	j = 0;
-	len_s1 = 0;
-	if (s1 != NULL)
-		len_s1 = ft_strlen(s1);
-	str3 = (char *)malloc(len_s1 + ft_strlen(s2) + 1);
+	str3 = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str3)
 		return (NULL);
 	while (s1 != NULL && s1[i])
